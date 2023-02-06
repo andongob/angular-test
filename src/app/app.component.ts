1dbaf8d7c3fe302651d59d5c35fcf0d9a6143f2f
andongob <ndongo@gmail.com>
2023-02-06 19:04:03 +0100

    sesión iniciada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { FormBuilder } from '@angular/forms';
 
 import * as Mnemonic from "bitcore-mnemonic";
 import * as CryptoJS from "crypto-js";
+import * as bip39 from "bip39";
+import { hdkey } from 'ethereumjs-wallet';
+import * as util from "ethereumjs-util";
 
 
 @Component({
@@ -15,7 +18,11 @@ export class AppComponent {
 
   loginForm: any;
 
-  encrypted: any
+  encrypted: any;
+
+  wallet:any = {
+    address: ''
+  }
 
   constructor(private formBuilder: FormBuilder){
     this.loginForm = this.formBuilder.group ({
@@ -24,21 +31,46 @@ export class AppComponent {
     });
 
   this.encrypted = window.localStorage.getItem('seeds');  //valida que las semillas existen y están en localStorage
+
+  this.initWallet('feature lesson crowd eager guitar exhibit memory degree pride hole shine battle') //FUERA DE CÓDIGO: truquillo para que mientras desarrollemos no tengamos que meter todo el rato la contraseña:
   }
 
 //feature lesson crowd eager guitar exhibit memory degree pride hole shine battle - pwd: seña
 
-  /*sendLogin(loginData:any){ 
+async initWallet(seeds: string) {  //método para inciar el wallet en ethereum
+  var mnemonic = new Mnemonic(seeds);
+  var seed = await bip39.mnemonicToSeed(mnemonic.toString());
+  var path = "m/44'/60'/0'70/0";
+
+  var wallet = hdkey
+  .fromMasterSeed(seed)
+  .derivePath(path)
+  .getWallet();
+
+  var privateKey = wallet.getPrivateKey();
+  var publicKey = util.privateToPublic(privateKey);
+  var address = "0x" + util.pubToAddress(publicKey).toString("hex"); //convierte las palabras clave en semillas
+
+  this.wallet.address = address;  //indica la cuenta address, del div Hello en app.component.html una vez iniciada sesión
+
+  //console.log(address);
+  //console.log(privateKey);
+  //console.log(publicKey);
+
+
+}
+
+  /*sendLogin(loginData:any){ //pide semillas y contraseña
     if (loginData.seeds == '' || loginData.password == ''){
       return alert('Campos vacíos, por favor introduce credenciales'); //aparece si alguno de los campos está vacío
     } */
 
- sendLogin(loginData:any){ 
+ sendLogin(loginData:any){ //solo pide contraseña
     if (loginData.password == ''){
       return alert('Por favor, introduce tu contraseña'); 
     }
 
-    if (this.encrypted) {//guardadas las semillas en el localStorage solo solicita password
+    if (this.encrypted) {//guardadas las semillas en el localStorage solo solicita password - min 24:00
       var decrypt = CryptoJS.AES.decrypt(this.encrypted, loginData.password);
       loginData.seeds = decrypt.toString(CryptoJS.enc.Utf8);
     }
@@ -47,14 +79,17 @@ export class AppComponent {
       return alert('Semilla inválida') //aparece si la semilla es incorrecta
     }
 
-    console.log(loginData);
+    //console.log(loginData);
 
     var encrypted = CryptoJS.AES.encrypt(loginData.seeds, loginData.password).toString();
 
-    console.log(encrypted);
+    //console.log(encrypted);
 
     window.localStorage.setItem('seeds', encrypted);
 
+    this.loginForm.reset(); //reinicia el formulario
+
+    this.initWallet(loginData.seeds);
 
   }
 }
