cb037b8cffb1264ed88d38fd80e6d284d2bf85a2
andongob <ndongo@gmail.com>
2023-02-24 22:52:57 +0100

    wallet connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 import * as Mnemonic from "bitcore-mnemonic";
 import * as CryptoJS from "crypto-js";
+import { hdkey } from "ethereumjs-wallet";
 import * as bip39 from "bip39";
-import { hdkey } from 'ethereumjs-wallet';
 import * as util from "ethereumjs-util";
 import Web3 from 'web3';
-
 
 @Component({
   selector: 'app-root',
@@ -17,7 +16,6 @@ import Web3 from 'web3';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-ndg';
 
   loginForm: any;
 
@@ -34,12 +32,14 @@ export class AppComponent {
 
   window: any;
 
-  constructor(@Inject(DOCUMENT) private document: Document, private formBuilder: FormBuilder){ //Angular no reconoce windows.ethereum hay que 'engañarle' con windows.document
+  mining = false;
+
+  lastTransaction: any;
+
+  constructor(@Inject(DOCUMENT) private document: Document, private formBuilder: FormBuilder) {
     this.window = document.defaultView;
 
-    //console.log(this.window.ethereum);
-    
-    this.loginForm = this.formBuilder.group ({
+    this.loginForm = this.formBuilder.group({
       seeds: '',
       password: ''
     });
@@ -49,108 +49,86 @@ export class AppComponent {
       amount: ''
     });
 
+    this.encrypted = window.localStorage.getItem('seeds');
 
-  this.encrypted = window.localStorage.getItem('seeds');  //valida que las semillas existen y están en localStorage
+    //this.initWallet('february current defy one inform wet hurry cupboard type enable spare famous'); // trampa
 
-  //this.initWallet('feature lesson crowd eager guitar exhibit memory degree pride hole shine battle') //FUERA DE CÓDIGO: truquillo para que mientras desarrollemos no tengamos que meter todo el rato la contraseña:
-  
-  this.web3 = new Web3;
+    //this.web3 = new Web3;
+	
+	this.web3 = new Web3(this.window.ethereum); 
 
-  this.web3.setProvider( 
-    new this.web3.providers.HttpProvider('https://goerli.infura.io/v3/87388b2cafcd4bcdbb26947767a1869f')
-  );
-}
+	/*
+    this.web3.setProvider(
+      new this.web3.providers.HttpProvider('https://ropsten.infura.io/v3/d09825f256ae4705a74fdee006040903')
+    );
+	*/
+  }
 
-//feature lesson crowd eager guitar exhibit memory degree pride hole shine battle - pwd: seña
+  // february current defy one inform wet hurry cupboard type enable spare famous
+  async initWallet(seeds: string) {
+    var mnemonic = new Mnemonic(seeds);
+    var seed = await bip39.mnemonicToSeed(mnemonic.toString());
+    var path = "m/44'/60'/0'/0/0";
 
-async initWallet(seeds: string) {  //método para inciar el wallet en ethereum con semillas
-  var mnemonic = new Mnemonic(seeds);
-  var seed = await bip39.mnemonicToSeed(mnemonic.toString());
-  var path = "m/44'/60'/0'70/0";
+    var wallet = hdkey
+      .fromMasterSeed(seed)
+      .derivePath(path)
+      .getWallet();
 
-  var wallet = hdkey
-  .fromMasterSeed(seed)
-  .derivePath(path)
-  .getWallet();
+    var privateKey = wallet.getPrivateKey();
+    var publicKey = util.privateToPublic(privateKey);
+    var address = "0x" + util.pubToAddress(publicKey).toString("hex");
 
-  var privateKey = wallet.getPrivateKey();
-  var publicKey = util.privateToPublic(privateKey);
-  var address = "0x" + util.pubToAddress(publicKey).toString("hex"); //convierte las palabras clave en semillas
+    this.wallet.privateKey = privateKey;
 
-  this.wallet.privateKey = privateKey;
+    this.getBalance(address);
+    this.wallet.address = address; //indica la cuenta address, del div Hello en app.component.html una vez iniciada sesión
 
-  this.getBalance(address);
-  
-  this.wallet.address = address; //indica la cuenta address, del div Hello en app.component.html una vez iniciada sesión
-
-  this.wallet.balance = await this.web3.eth.getBalance(address).then((result:any) => {
+ this.wallet.balance = await this.web3.eth.getBalance(address).then((result:any) => {
     return this.web3.utils.fromWei(result, 'ether'); // convierte el balance de Wei a Ether
   });
-  //console.log(address);
-  //console.log(privateKey);
-  //console.log(publicKey);
- 
+  }
 
-}
-
-  /*sendLogin(loginData:any){ //pide semillas y contraseña
-    if (loginData.seeds == '' || loginData.password == ''){
-      return alert('Campos vacíos, por favor introduce credenciales'); //aparece si alguno de los campos está vacío
-    } */
-
-    async getBalance(address:string){ //si iniciamos con metamask
-
-    this.wallet.address = address; //indica la cuenta address, del div Hello en app.component.html una vez iniciada sesión
+  async getBalance(address:string) {
+    this.wallet.address = address;
     this.wallet.balance = await this.web3.eth.getBalance(address).then((result:any) => {
-      return this.web3.utils.fromWei(result, 'ether'); // convierte el balance de Wei a Ether
-  });
+      return this.web3.utils.fromWei(result, 'ether');
+    });
+  }
+
+  sendLogin(loginData:any) {
+    if (loginData.password == '') {
+      return alert('Introduce tu contraseña');
     }
 
-
- sendLogin(loginData:any){ //solo pide contraseña
-    if (loginData.password == ''){
-      return alert('Por favor, introduce tu contraseña'); 
-    }
-
-    if (this.encrypted) {//guardadas las semillas en el localStorage solo solicita password - min 24:00
+    if (this.encrypted) {
       var decrypt = CryptoJS.AES.decrypt(this.encrypted, loginData.password);
       loginData.seeds = decrypt.toString(CryptoJS.enc.Utf8);
     }
 
     if (!Mnemonic.isValid(loginData.seeds)) {
-      return alert('Semilla inválida') //aparece si la semilla es incorrecta
+      return alert('Semilla inválida');
     }
-
-    //console.log(loginData);
 
     var encrypted = CryptoJS.AES.encrypt(loginData.seeds, loginData.password).toString();
 
-    //console.log(encrypted);
-
     window.localStorage.setItem('seeds', encrypted);
 
-    this.loginForm.reset(); //reinicia el formulario
+    this.loginForm.reset();
 
-    this.initWallet(loginData.seeds);
-
+    this.initWallet(loginData.seed);
   }
 
-  loginWithMetamask() {  //método para hacer login con metamask, y se abre la ventana de metamask, curiosamente si tienes otro wallet te coje coinbase wallet en mi caso 
+  loginWithMetamask() {
     if (!this.window.ethereum) {
-      return alert('No tienes instalado Metamask');
+      return alert('Metamask no está instalado');
     }
 
-    //El siguente método se usa para que muestre el balance de la address en caso de loguear con Metamask
-
-    this.window.ethereum.enable().then((accounts:any) => {  //para que funcione hay que 'trampear con window.document
-      let address = accounts[0]; 
+    this.window.ethereum.enable().then((accounts:any) => {
+      let address = accounts[0];
       this.getBalance(address);
-
-
-      console.log(accounts)
+	  this.wallet.address = address;
     });
-
-    //console.log(this.window.ethereum)
   }
 
   removeSeeds() {
@@ -162,29 +140,45 @@ async initWallet(seeds: string) {  //método para inciar el wallet en ethereum c
     };
   }
 
-async sendEther(sendData:any){
-if (sendData.to == '' || sendData.amount == null) {
-  return alert('Algún campo está vacío')
-}
+  async sendEther(sendData:any) {
+    if (sendData.to == '' || sendData.amount == null) {
+      return alert('Campos son obligatorios');
+    }
 
-if ( ! util.isValidAddress(sendData.to)) {
-  return alert('La dirección no es válida');
-}
+    if ( ! util.isValidAddress(sendData.to)) {
+      return alert('Dirección inválida');
+    }
 
-// en las siguentes lineas método de transacción
-var rawData = {
-  from: this.wallet.address,  //usuario origen, es decir, nuestra wallet
-  to: sendData.to,
-  value: sendData.amount,
-  gasPrice: this.web3.utils.toHex(10000000000),
-  gasLimit: this.web3.utils.toHex(1000000),
-  nonce: await this.web3.eth.getTransactionCount(this.wallet.address)
-};
+    this.mining = true;
 
-var signed = await this.web3.eth.accounts.signTransaction(rawData, this.wallet.privateKey.toString("hex"));
+    var rawData = {
+      from: this.wallet.address,
+      to: sendData.to,
+      value: sendData.amount,
+      gasPrice: this.web3.utils.toHex(10000000000),
+      gasLimit: this.web3.utils.toHex(1000000),
+      //nonce: await this.web3.eth.getTransactionCount(this.wallet.address)
+    };
 
-    console.log(signed);
+	var signed = await this.web3.eth.sendTransaction(rawData).then((receipt:any) => {
+	
+  	  console.log("Transaction succeeded", receipt);		
+      this.mining = false;
+      this.lastTransaction = receipt;
 
-    //console.log(sendData);
+      this.sendForm.reset();
+    });
+	
+    //var signed = await this.web3.eth.accounts.signTransaction(rawData, this.wallet.privateKey.toString('hex'));
+
+	/*
+    this.web3.eth.sendSignedTransaction(signed.rawTransaction).then((receipt:any) => {
+      this.mining = false;
+      this.lastTransaction = receipt;
+
+      this.sendForm.reset();
+    });
+	*/
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
